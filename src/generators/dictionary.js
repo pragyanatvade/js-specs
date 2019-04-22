6c9ec21e118b315d5008f290120a837826633c98
Pragyan Tripathi <pntripathi9417@gmail.com>
2019-04-23 05:21:41 +0530

    feat(generators): add generators for dictionary

---
+++ src/generators/dictionary.js
@@ -0,0 +1,22 @@
+export const canGenerateDictionary = ({ tuple, set, map }) => {
+  const iterator = (items) => {
+    const obj = {};
+    for (const item of items) { // eslint-disable-line
+      const [key, value] = item;
+      obj[key] = value;
+    }
+    return obj;
+  };
+
+  const dictionary = (params) => {
+    const { key, value } = params || {};
+    if (!key) throw new Error('dictionary arbitrary expects valid arbitrary to generate key');
+    if (!value) throw new Error('dictionary arbitrary expects valid arbitrary to generate values');
+    const arb = set({ arb: tuple([key, value]), compare: (t1, t2) => t1[0] === t2[0] });
+    return map(arb, iterator);
+  };
+
+  return ({ dictionary, dict: dictionary });
+};
+
+export default canGenerateDictionary;
