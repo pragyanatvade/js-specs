8e43342dfe3de741b46fe4311f5cd52d06c0a474
Pragyan Tripathi <pntripathi9417@gmail.com>
2019-04-23 05:17:28 +0530

    feat(generators): add string generators

---
+++ src/generators/string.js
@@ -0,0 +1,26 @@
+const stringArbitrary = ({
+  min, max, map, arb, array
+}) => {
+  const iterator = val => val.join('');
+  return map(array({ arb, min, max }), iterator);
+};
+
+export const canGenerateString = ({ char, array, map }) => {
+  const string = (params) => {
+    const { min, max = params } = params || {};
+    return stringArbitrary({
+      min, max, arb: char(), array, map
+    });
+  };
+  return ({ string });
+};
+
+export const canGenerateAsciiString = ({ ascii, array, map }) => {
+  const asciiString = (params) => {
+    const { min, max = params } = params || {};
+    return stringArbitrary({
+      min, max, arb: ascii(), array, map
+    });
+  };
+  return ({ asciiString });
+};
