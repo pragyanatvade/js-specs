bfeb2e1dccf3fb36e657eef398be62d9afdba1ce
Pragyan Tripathi <pntripathi9417@gmail.com>
2019-04-23 10:09:19 +0530

    tests(generators): add tests for option generators

---
+++ src/generators/__tests__/options.spec.js
@@ -0,0 +1,22 @@
+import generators from '..';
+import helpers from '../../helpers';
+
+const { integer, option } = generators;
+const { randomBySeed: rand } = helpers;
+
+describe('generators', () => {
+  describe('option', () => {
+    describe('generate', () => {
+      it('When integer arbitrary is passed, should generate number and null sometimes', () => {
+        const seed = Date.now();
+        const min = 0;
+        const max = 10;
+        const intArb = integer({ min, max });
+        const arb = option(intArb);
+        const { value } = arb.generate(rand(seed));
+        const check = value >= min || value <= max || value === null;
+        expect(check).toBeTruthy();
+      });
+    });
+  });
+});
