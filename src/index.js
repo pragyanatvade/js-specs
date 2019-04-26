ae3e922718dbe1cfd8bf98922cc9efee6335eb3e
Pragyan Tripathi <pntripathi9417@gmail.com>
2019-04-26 17:39:22 +0530

    feat(vade-spec): expose generators, predicates, properties, specs, transformers modules

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import generators from './generators';
+import helpers from './helpers';
+import predicates from './predicates';
+import properties from './properties';
+import specs from './specs';
+import transformers from './transformers';
+
+export default {
+  generators, helpers, predicates, properties, specs, transformers
+};
