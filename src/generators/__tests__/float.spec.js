a71cd5f289dcaa40c168bf4e11c883784c21ab8d
Pragyan Tripathi <pntripathi9417@gmail.com>
2019-04-27 19:01:56 +0530

    tests(generators): fix tests for float and double generators

--- src/generators/__tests__/float.spec.js
+++ src/generators/__tests__/float.spec.js
@@ -8,21 +8,21 @@ describe('generators', () => {
   describe('float', () => {
     describe('generate', () => {
       it('When generate is called, Should generate a float', () => {
-        const seed = 1020;
+        const seed = Date.now();
         const arb = float();
         const { value } = arb.generate(rand(seed));
-        expect(value).toBeLessThan(1);
-        expect(value).toBeGreaterThan(0);
+        expect(value).toBeLessThanOrEqual(1);
+        expect(value).toBeGreaterThanOrEqual(0);
       });
     });
     describe('shrink', () => {
       it('When arbitraries are passed, Should shrink a tuple', () => {
-        const seed = 1020;
+        const seed = Date.now();
         const arb = float();
         const { shrink } = arb.generate(rand(seed));
         shrink().every(({ value }) => {
-          expect(value).toBeLessThan(1);
-          expect(value).toBeGreaterThan(0);
+          expect(value).toBeLessThanOrEqual(1);
+          expect(value).toBeGreaterThanOrEqual(0);
           return true;
         });
       });
@@ -31,21 +31,21 @@ describe('generators', () => {
   describe('double', () => {
     describe('generate', () => {
       it('When generate is called, Should generate a float', () => {
-        const seed = 1020;
+        const seed = Date.now();
         const arb = double();
         const { value } = arb.generate(rand(seed));
-        expect(value).toBeLessThan(1);
-        expect(value).toBeGreaterThan(0);
+        expect(value).toBeLessThanOrEqual(1);
+        expect(value).toBeGreaterThanOrEqual(0);
       });
     });
     describe('shrink', () => {
       it('When arbitraries are passed, Should shrink a tuple', () => {
-        const seed = 1020;
-        const arb = float();
+        const seed = Date.now();
+        const arb = double();
         const { shrink } = arb.generate(rand(seed));
         shrink().every(({ value }) => {
-          expect(value).toBeLessThan(1);
-          expect(value).toBeGreaterThan(0);
+          expect(value).toBeLessThanOrEqual(1);
+          expect(value).toBeGreaterThanOrEqual(0);
           return true;
         });
       });
