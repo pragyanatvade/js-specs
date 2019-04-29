dee8be98679a2e698ceeeca265f8ce56570c8893
Pragyan Tripathi <pntripathi9417@gmail.com>
2019-04-29 11:04:18 +0530

    tests(specs): add tests for collOf spec

--- src/specs/__tests__/coll-of.spec.js
+++ src/specs/__tests__/coll-of.spec.js
@@ -3,11 +3,17 @@ import preds from '../../predicates';
 
 describe('specs', () => {
   describe('collOf', () => {
-    it('should work', () => {
+    it('When pass even predicate and list of integer, Should return invalid', () => {
       specs.def({ key: 'even', predicate: preds.even });
       specs.def({ key: 'even-coll', predicate: specs.collOf(preds.even) });
       const resp = specs.conform({ key: 'even-coll', data: [1, 2, 3] });
-      console.log(resp);
+      expect(resp).toEqual(preds.invalid());
+    });
+    it('When pass even predicate and list of even numbers, Should return conformed value', () => {
+      specs.def({ key: 'even', predicate: preds.even });
+      specs.def({ key: 'even-coll', predicate: specs.collOf(preds.even) });
+      const resp = specs.conform({ key: 'even-coll', data: [2, 4, 6] });
+      expect(resp).toEqual([2, 4, 6]);
     });
   });
 });
