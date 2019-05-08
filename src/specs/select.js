ce148a5291147680ea7f69950111a17fc359c043
Pragyan Tripathi <pntripathi9417@gmail.com>
2019-05-08 09:43:52 +0530

    feat(specs): add select method in specs module

--- src/specs/select.js
+++ src/specs/select.js
@@ -1,36 +1,48 @@
-// export const canSelect = ({
-//   specTransducer,
-//   registry,
-//   predicates: { isString },
-//   transduce: {
-//     map, reduce, transduce
-//   }
-// }) => {
-//   const keyReducer = selection => () => (acc, spec) => {
-//     const { keys } = spec;
-//     console.log(spec);
-//     console.log(keys(), selection);
-//   };
-//   const select = (...args) => {
-//     let [{ schema, selection }] = args;
-//     if (args.length > 1) [schema, selection] = args;
+export const canSelect = ({
+  _,
+  registry,
+  transduce: {
+    map, into, filter
+  }
 
+}) => {
+  const conformReducer = ({ items }) => (params) => {
+    const reducer = (acc, item) => {
+      const { data = params } = params || {};
+      const { conform } = registry.get(item);
+      const value = conform({ key: item, data: data[item] });
+      _.set(acc, item, value);
+      return acc;
+    };
+    return _.reduce(items, reducer, {});
+  };
 
-//     const spec = registry.get(schema);
-//     const schemaKeys = spec.keys();
+  const predicateReducer = ({ items }) => ({ data }) => {
+    const reducer = (acc, item) => {
+      const { predicate } = registry.get(item);
+      const value = predicate(data[item]);
+      _.set(acc, item, value);
+      return acc;
+    };
+    return _.reduce(items, reducer, {});
+  };
 
-//     console.log(schemaKeys);
-//     // console.log('test', schema, selection);
-//     // const keys = specTransducer({ items: [schema],
-// reducer: keyReducer(selection), init: [] });
+  const select = (...args) => {
+    let [{ schema, selection }] = args;
+    if (args.length > 1) [schema, selection] = args;
+    const spec = registry.get(schema);
+    const schemaKeys = spec.keys();
 
-//     return ({});
-//     // if (isString(schema)) {
-//     //   const spec = registry.get(schema);
-//     //   console.log(, selection);
-//     // }
-//   };
-//   return ({ select });
-// };
+    const selectKeys = into({}, map(key => [key, true]), selection);
+    const items = into([], filter(key => selectKeys[key] === true), schemaKeys);
+    const conform = conformReducer({ items });
+    const predicate = predicateReducer({ items });
 
-// export default canSelect;
+    return ({
+      keys: () => items, conform, predicate, valid: predicate, isValid: predicate
+    });
+  };
+  return ({ select });
+};
+
+export default canSelect;
